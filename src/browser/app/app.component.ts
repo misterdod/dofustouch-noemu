39f045be88001f4f6aa5f0284e59c2ce3f36b081
Scapain <dofustouch@no-emu.com>
2016-12-29 18:36:05 +0100

    add action from electron (switch-tab and new-tab)

--- src/browser/app/app.component.ts
+++ src/browser/app/app.component.ts
@@ -1,6 +1,10 @@
-import {Component, Optional, ViewEncapsulation, Inject} from '@angular/core';
+import {Component, Optional, ViewEncapsulation, Inject, Input, NgZone} from '@angular/core';
 import {TabService} from './tab/tab.service';
 import {Tab} from './tab/tab';
+import { ShortCuts } from './shortcuts/shortcuts';
+import { IpcRendererService } from './electron/ipcrenderer.service';
+
+//const { ipcRenderer } = (<any>global).nodeRequire('electron');
 
 @Component({
     selector: 'application',
@@ -8,12 +12,15 @@ import {Tab} from './tab/tab';
     styleUrls: ['app/app.component.css'],
 })
 export class AppComponent {
-    lastDialogResult: string;
     tabs: Tab[];
     activTab: Tab = null;
+    private shortCuts: ShortCuts;
 
     constructor(
-        private tabService: TabService
+        @Inject('Window') private window: Window,
+        private tabService: TabService,
+        private ipcRendererService: IpcRendererService,
+        private zone: NgZone
     ) {
 
     }
@@ -28,11 +35,12 @@ export class AppComponent {
         this.tabService.addTab(tab);
 
         this.selectTab(tab);
+        this.getTabs();
     }
 
     removeTab(tab: Tab): void {
 
-        if(this.activTab !== null && tab.id === this.activTab.id){
+        if (this.activTab !== null && tab.id === this.activTab.id) {
             console.log('activTab was deleted');
             this.activTab = null;
             //let newTab = this.tabService.getNearTab(tab);
@@ -42,17 +50,60 @@ export class AppComponent {
         this.tabService.removeTab(tab);
     }
 
+    setEventListener(): void {
+
+        // ipc new tab
+        this.ipcRendererService.on('new-tab', (event: Event) => {
+            this.addTab();
+        });
+
+        // ipc switch tab
+        this.ipcRendererService.on('switch-tab', (event: Event, action: string | number) => {
+            console.log('switch tab');
+            console.log(action);
+            if ((<any>Number).isInteger(action)) {
+                this.selectTab(this.tabs[action]);
+            } else {
+                let index = this.tabs.indexOf(this.activTab);
+                switch (action) {
+                    case 'prev':
+                        if (index !== -1) {
+                            if (index === 0) {
+                                this.selectTab(this.tabs[this.tabs.length - 1]);
+                            } else {
+                                this.selectTab(this.tabs[index - 1]);
+                            }
+                        }
+                        break;
+                    case 'next':
+                        if (index !== -1) {
+                            if (index === (this.tabs.length - 1)) {
+                                this.selectTab(this.tabs[0]);
+                            } else {
+                                this.selectTab(this.tabs[index + 1]);
+                            }
+                        }
+                        break;
+                }
+            }
+        });
+    }
+
     selectTab(tab: Tab): void {
-        if(this.activTab !== null){
+        if (this.activTab !== null) {
             this.activTab.isFocus = false;
         }
 
         tab.isFocus = true;
         this.activTab = tab;
+        console.log(tab);
     }
 
     ngOnInit(): void {
         this.getTabs();
+
+        this.shortCuts = new ShortCuts(this.window);
+        this.setEventListener();
         //this.addTab();
     }
 }
