8f35894c475d5e82bb70099878296719fe781b31
Scapain <dofustouch@no-emu.com>
2017-01-04 17:57:36 +0100

    add input component for shortcut option

--- src/browser/app/option/shortcuts/input/input.component.ts
+++ src/browser/app/option/shortcuts/input/input.component.ts
@@ -1,4 +1,5 @@
 import {Component, Optional, ViewEncapsulation, Inject, Input, Output, NgZone, EventEmitter} from '@angular/core';
+import { KeyCode } from './keycode';
 
 
 //const { ipcRenderer } = (<any>global).nodeRequire('electron');
@@ -17,6 +18,42 @@ export class InputComponent {
     @Input() private id: string;
     @Input() private model: any;
     @Output() modelChange: EventEmitter<any> = new EventEmitter();
+
+    private _keys: Array<number> = [];
+
+    keyDown(event: KeyboardEvent) {
+        event.preventDefault();
+
+        this._keys.forEach((key: number, index) => {
+            if(key == event.keyCode){
+                delete this._keys[index];
+            }
+        });
+
+        this._keys.push(event.keyCode);
+
+        let first = true;
+        let shortcut = '';
+
+        this._keys.forEach((key) => {
+            if (this._keys.length > 1 && !first) {
+                shortcut += '+';
+            }
+
+            console.log(key);
+            shortcut += KeyCode.getKeyCodeValue(key, false);
+
+            first = false;
+        });
+
+
+        this.model = shortcut;
+        this.modelChange.emit(this.model);
+    }
+
+    keyUp(event: KeyboardEvent) {
+        delete  this._keys[this._keys.indexOf(event.keyCode)];
+    }
 
     constructor(
     ) {
